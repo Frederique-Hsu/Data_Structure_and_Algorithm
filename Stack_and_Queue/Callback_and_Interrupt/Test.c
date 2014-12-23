9bb2a1146b25f2944ac45fefabbf7452d45e4625
XU ZAN <zan.xu@hella.com>
2014-12-23 14:41:19 +0800

    增加3個程序文件 Callback_and_Interrupt.c  Test.c  Test.h

---
+++ Stack_and_Queue/Callback_and_Interrupt/Test.c
@@ -0,0 +1,22 @@
+/********************************************************************************************
+ * File name		: Test.c
+ * Description		: Perform the test cases here.
+ * Creator			: XU ZAN
+ * Creation date	: Mon.	Dec. 22, 2014
+ * Copyright(C)		2014	All rights reserved.
+ *
+ ********************************************************************************************/
+
+
+#include "Test.h"
+
+void General_Test_Cases(void)
+{
+	TestCase_CallbackMechanism();
+
+	return;
+}
+
+void TestCase_CallbackMechanism(void)
+{
+}
