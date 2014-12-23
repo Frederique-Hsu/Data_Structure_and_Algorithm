9c5927571cdb801ba67b55f9293db605db98fe89
XU ZAN <zan.xu@hella.com>
2014-12-23 14:51:35 +0800

    什麽是回調? 怎樣定義和實現回調函數? 怎麽樣調用回調函數?  通過函數指針來指向和調用回調函數.

---
+++ Stack_and_Queue/Callback_and_Interrupt/Callback/Callback.c
@@ -0,0 +1,28 @@
+/***************************************************************************************
+ * File name		: Callback.c
+ * Description		: Study the call-back mechanism, how to program the call-back in C.
+ * Creator			: XU ZAN
+ * Creation date	: Mon.	Dec. 22, 2014
+ * Copyright(C)		2014	All rights reserved.
+ *
+ ***************************************************************************************/
+
+#include "Callback.h"
+
+#include <stdio.h>
+
+void GetCallBack(pCallBack callback)
+{
+	printf("This function is a caller.\n");
+}
+
+void Demo_Call_Back_Mechanism(void)
+{
+	printf("Demonstrate how to implement the call-back functions.\n");
+	GetCallBack(fCallback);
+}
+
+void fCallback(char *s)
+{
+	printf("This function is a callee which is defined by user.\n");
+}
