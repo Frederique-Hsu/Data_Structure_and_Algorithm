634b0c3a138e5246ce285b222d0efad4de957ade
xuzan <zan.xu@hella.com>
2015-04-09 10:29:40 +0800

    演示与测试汉诺塔问题求解。

---
+++ Stack_and_Queue/Stack_and_Queue/Stack/Test_Hanoi.h
@@ -0,0 +1,16 @@
+/*******************************************************************************************
+ * File name		: Test_Hanoi.h
+ * Description		: Demonstrate and validate the Hanoi tower disk stack.
+ * Creator			: Frederick Hsu
+ * Creation date	: Thur.	April 02, 2015
+ * Copyright(C)		2015	All rights reserved.
+ *
+ *******************************************************************************************/
+
+
+#ifndef TEST_HANOI_H
+#define TEST_HANOI_H
+
+	void Demonstrate_Hanoi_Tower(void);
+
+#endif	/*  TEST_HANOI_H  */
