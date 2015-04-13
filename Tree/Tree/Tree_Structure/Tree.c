95c3f3a133ec8a2e336f3aaa0a77b0d4dc04b78f
Frédérick Hsu <frederick.hsu@icloud.com>
2015-04-13 21:03:58 +0800

    Define and implement the functions :  int CreateBiTree(BiTree *T),    int DisplayBiTree(BiTree T),  void BiTreeTraverse(BiTree T, void (*VisitNode)(BiTree T))
    創建二叉樹和打印二叉樹各結點.

--- Tree/Tree/Tree_Structure/Tree.c
+++ Tree/Tree/Tree_Structure/Tree.c
@@ -9,3 +9,89 @@
 
 
 #include "Tree.h"
+#include <string.h>
+#include <stdlib.h>
+
+
+int PreOrderTraverse(BiTree T, int (*visit)(TElemType e))
+/* 採用二叉鏈表存儲結構, visit是對數据元素操作的應用函數.
+ * 先序遍歷二叉樹T的遞歸算法,對每個數據元素調用函數visit.
+ */
+{
+	if (T)
+	{
+		if (visit(T->data))
+		{
+			if (PreOrderTraverse(T->lchild, visit))
+			{
+				if (PreOrderTraverse(T->rchild, visit))
+				{
+					return OK;
+				}
+			}
+		}
+		return ERROR;
+	}
+	else
+	{
+		return OK;
+	}
+}
+
+int DisplayElement(TElemType e)
+{
+	printf("%d\t\n", e);
+	return OK;
+}
+
+int CreateBiTree(BiTree *T)
+/* 按先序次序輸入二叉樹中結點的值，構造二叉鏈表表示的二叉樹T.
+ */
+{
+	int n;
+	char sNum[16] = {0};
+	printf("Please enter a number for one tree node (null to terminate): ");
+	scanf("%s", sNum);
+	if (!stricmp(sNum, "null"))
+	{
+		(*T) = NULL;
+	}
+	else
+	{
+		n = atoi(sNum);
+		if (NULL == ((*T) = (BiTree)calloc(1, sizeof(BiTNode))))
+		{
+			return OVERFLOW;
+		}
+		(*T)->data = n;
+		CreateBiTree(&((*T)->lchild));
+		CreateBiTree(&((*T)->rchild));
+	}
+	return OK;
+}
+
+int DisplayBiTree(BiTree T)
+{
+	printf(" Current	| Data	| L_Child	| R_Child	|\n");
+	printf("-------------------------------------------------------------------------\n");
+	BiTreeTraverse(T, PrintBiTreeNode);
+	return OK;
+}
+
+void BiTreeTraverse(BiTree T, void (*VisitNode)(BiTree T))
+{
+	if (T)
+	{
+		VisitNode(T);
+		BiTreeTraverse(T->lchild, VisitNode);
+		BiTreeTraverse(T->rchild, VisitNode);
+	}
+}
+
+void PrintBiTreeNode(BiTree T)
+{
+	printf(" 0x%08X	| %d	| 0x%08X	| 0x%08X	|\n", 
+		   T, T->data, T->lchild, T->rchild);
+	printf("-------------------------------------------------------------------------\n");
+	return;
+}
--- Tree/Tree/Tree_Structure/Tree.h
+++ Tree/Tree/Tree_Structure/Tree.h
@@ -11,5 +11,49 @@
 #ifndef TREE_H
 #define TREE_H
 
+	#include "universal_definitions.h"
+	#include <stdio.h>
+
+	typedef int			TElemType;			// 樹結點的元素的數據類型
+
+	/*********二叉樹的順序存儲表示**********/
+	#define MAX_TREE_SIZE	100				// 二叉樹的最大結點數
+
+	typedef TElemType	SqBiTree[MAX_TREE_SIZE];	// 0號單元存儲根結點
+
+	SqBiTree bt;
+
+#if 1
+	/*********二叉樹的二叉鏈表存儲表示************/
+	typedef struct BiTNode
+	{
+		TElemType data;
+		struct BiTNode *lchild;
+		struct BiTNode *rchild;
+	}
+	BiTNode, *BiTree;
+#else
+	/*********二叉樹的三叉鏈表存儲表示************/
+	typedef struct BiTNode
+	{
+		TElemType data;
+		struct BiTNode *lchild;
+		struct BiTNode *rchild;
+		struct BiTNode *parent;
+	}
+	BiTNode, *BiTree;
+#endif
+
+	int CreateBiTree(BiTree *T);
+	int DisplayBiTree(BiTree T);
+	int PreOrderTraverse(BiTree T, int (*visit)(TElemType e));		// 先序遍歷
+	int InOrderTraverse(BiTree T, int (*visit)(TElemType e));		// 中序遍歷
+	int PostOrderTraverse(BiTree T, int (*visit)(TElemType e));		// 後序遍歷
+	int LevelOrderTraverse(BiTree T, int (*visit)(TElemType e));	// 層序遍歷
+
+	int DisplayElement(TElemType e);		// 簡單的 visit 回調函數
+
+	void BiTreeTraverse(BiTree T, void (*VisitNode)(BiTree T));
+	void PrintBiTreeNode(BiTree T);
 
 #endif	/*  TREE_H  */
