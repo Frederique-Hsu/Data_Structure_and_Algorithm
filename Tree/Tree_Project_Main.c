dfbaf2a9e36e07b14a82d061c2811abef0739e89
徐贊_Frederick_Hsu <frederick.hsu@icloud.com>
2015-12-30 15:56:46 +0800

    創建一個新項目，完全使用Makefile來管理項目，使用gcc工具鏈來編譯，調試。
    通過coding.net 的 WebIDE來實現完全在綫編程開發，一個瀏覽器就能搞定。

---
+++ Tree/Tree_Project_Main.c
@@ -0,0 +1,23 @@
+/******************************************************************************************************************
+ * Project name     : Tree
+ * Description      : Try to do programming in the WebIDE environment, which is provided by Coding.net
+ *                    Create a new project to study the tree structure and its algorithm.
+ * ================================================================================================================
+ * File name        : Tree_Project_Main.c
+ * Description      : This file will implement the main() entrance function for current project.
+ * Creator          : Frederick Hsu (徐贊)
+ * Creation date    : Wed.  Dec. 30, 2015
+ * Copyright(C)     2015    All rights reserved.
+ * 
+ ******************************************************************************************************************/
+
+
+#include <stdio.h>
+
+int main(int argc, char *argv[])
+{
+    
+/******************************/
+    printf("\n");
+    return 0;
+}
