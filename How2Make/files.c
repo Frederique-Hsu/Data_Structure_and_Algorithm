8940ec1eeb8d8ed4e292ac2bcb18ebef1bf46101
徐贊_Frederick_Hsu <frederick.hsu@icloud.com>
2016-02-24 00:59:23 +0800

    Edit all .c and .h files.

---
+++ How2Make/files.c
@@ -0,0 +1,13 @@
+/*******************************************************************************
+ * File name    : files.c
+ * Description  : 
+ * Author       : Frederick Hsu
+ * Creation date: Wed.  24 Feb., 2016
+ * Copyright(C) 2016    All rights reserved.
+ * 
+ *******************************************************************************/
+
+#include "defs.h"
+#include "buffer.h"
+#include "command.h"
+
---
+++ How2Make/search.c
@@ -0,0 +1,12 @@
+/*******************************************************************************
+ * File name    : search.c
+ * Description  : 
+ * Author       : Frederick Hsu
+ * Creation date: Wed.  24 Feb., 2016
+ * Copyright(C) 2016    All rights reserved.
+ * 
+ *******************************************************************************/
+
+#include "defs.h"
+#include "buffer.h"
+
