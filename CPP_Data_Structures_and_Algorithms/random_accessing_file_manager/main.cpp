2898239f74434a9942ec171205b63ba21c69a53b
Frederick Hsu (徐赞) <frederick.hsu@icloud.com>
2016-03-29 00:32:27 +0800

    Change the directory name from "random_access_file" to "random_accessing_file_manager", and all all .cpp and .h files.

---
+++ CPP_Data_Structures_and_Algorithms/random_accessing_file_manager/main.cpp
@@ -0,0 +1,17 @@
+/*************************************************************************************************
+ * Project		: random_accessing_file_manager
+ * Description	: 案例分析 － 管理隨機訪問文件
+ * ===============================================================================================
+ * File name	: main.cpp
+ * Description	: This file implements the entry function main() for current project.
+ * Creator		: Fredericks Hsu
+ * Creation date: Mon.	28 March, 2016
+ * Copyright(C)	2016	All rights reserved.
+ *
+ *************************************************************************************************/
+
+
+int main(int argc, char *argv[])
+{
+	return 0;
+}
