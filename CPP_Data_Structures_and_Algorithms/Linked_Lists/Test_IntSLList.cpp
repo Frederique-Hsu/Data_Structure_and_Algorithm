010b1f62ea850451fe52d09780f4b1e1cde13cbe
Frederick Hsu (徐赞) <frederick.hsu@icloud.com>
2016-06-27 15:42:31 +0800

    Add 2 new files Test_IntSLList.cpp (.h) into the project, in order to design the unit test cases to verify IntSLList structure.

---
+++ CPP_Data_Structures_and_Algorithms/Linked_Lists/Test_IntSLList.cpp
@@ -0,0 +1,12 @@
+/*************************************************************************************************
+ * File name    : Test_IntSLList.cpp
+ * Description  : Design some unit test cases to verify the Singly Linked Lists.
+ * Creator      : Frederick Hsu
+ * Creation date: Mon.  27 June, 2016
+ * Copyright(C) 2016    All rights reserved.
+ *
+ *************************************************************************************************/
+
+#include "Test_IntSLList.h"
+
+#include "intSLList.h"
---
+++ CPP_Data_Structures_and_Algorithms/Linked_Lists/Test_IntSLList.h
@@ -0,0 +1,14 @@
+/*************************************************************************************************
+ * File name    : Test_IntSLList.h
+ * Description  : Design some unit test cases to verify the Singly Linked Lists.
+ * Creator      : Frederick Hsu
+ * Creation date: Mon.  27 June, 2016
+ * Copyright(C) 2016    All rights reserved.
+ *
+ *************************************************************************************************/
+
+#ifndef TEST_INTSLLIST_H
+#define TEST_INTSLLIST_H
+
+
+#endif  /* TEST_INTSLLIST_H */
