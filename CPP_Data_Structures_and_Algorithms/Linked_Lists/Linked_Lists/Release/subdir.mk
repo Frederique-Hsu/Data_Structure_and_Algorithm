5f6330447ff65bddc757ef36937b70f558ee5a6f
Frederick Hsu (徐赞) <frederick.hsu@icloud.com>
2016-07-03 19:25:45 +0800

    Add the makefile and all .mk files, including the ones under the subdirectories.

---
+++ CPP_Data_Structures_and_Algorithms/Linked_Lists/Linked_Lists/Release/subdir.mk
@@ -0,0 +1,30 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+CPP_SRCS += \
+../Main.cpp \
+../data_types.cpp \
+../test.cpp 
+
+OBJS += \
+./Main.o \
+./data_types.o \
+./test.o 
+
+CPP_DEPS += \
+./Main.d \
+./data_types.d \
+./test.d 
+
+
+# Each subdirectory must supply rules for building sources it contributes
+%.o: ../%.cpp
+	@echo 'Building file: $<'
+	@echo 'Invoking: GCC C++ Compiler'
+	g++ -O3 -Wall -c -fmessage-length=0 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@)" -o "$@" "$<"
+	@echo 'Finished building: $<'
+	@echo ' '
+
+
