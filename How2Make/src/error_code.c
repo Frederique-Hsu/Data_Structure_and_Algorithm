23d4527d8b7f1dbd7d6554075b8462f08816e1c7
Frederick Hsu (徐赞) <frederick.hsu@icloud.com>
2016-07-17 20:48:30 +0800

    添加一個 src/error_code.c 源文件，注意其目錄結構層次。

---
+++ How2Make/src/error_code.c
@@ -0,0 +1,12 @@
+/*******************************************************************************
+ * File name    : error_code.c
+ * Description  : 
+ * Author       : Frederick Hsu
+ * Creation date: Sun.  17 JULY, 2016
+ * Copyright(C) 2016    All rights reserved.
+ * 
+ *******************************************************************************/
+
+
+
+#include "../headers/error_code.h"
