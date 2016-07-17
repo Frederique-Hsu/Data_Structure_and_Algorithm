41e460ac9173317d7814fa7271f73e02cef9df17
Frederick Hsu (徐赞) <frederick.hsu@icloud.com>
2016-07-17 22:55:25 +0800

    想要研究子目錄下的源文件怎麼編譯的， 但失敗了。待後面深入研究。

---
+++ How2Make/src/memcp.c
@@ -0,0 +1,8 @@
+/*******************************************************************************
+ * File name    : memcp.c
+ * Description  : 
+ * Author       : Frederick Hsu
+ * Creation date: Sun.  17 JULY, 2016
+ * Copyright(C) 2016    All rights reserved.
+ * 
+ *******************************************************************************/
