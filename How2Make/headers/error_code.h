53c7e1f425d90839551b04b62a6480fa64fd4953
Frederick Hsu (徐赞) <frederick.hsu@icloud.com>
2016-07-17 20:44:10 +0800

    增加三个代码文件 proc.c  headers/error_code.h   headers/type_definies.h
    注意其目录层次结构。

---
+++ How2Make/headers/error_code.h
@@ -0,0 +1,15 @@
+/*******************************************************************************
+ * File name    : error_code.h
+ * Description  : 
+ * Author       : Frederick Hsu
+ * Creation date: Sun.  17 JULY, 2016
+ * Copyright(C) 2016    All rights reserved.
+ * 
+ *******************************************************************************/
+
+
+#ifndef ERROR_CODE_H
+#define ERROR_CODE_H
+
+
+#endif  /* ERROR_CODE_H */
