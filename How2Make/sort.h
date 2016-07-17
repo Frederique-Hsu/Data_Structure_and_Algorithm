cac379cec3f386ec104f1cd16f0b01c3c86f6bb6
Frederick Hsu (徐赞) <frederick.hsu@icloud.com>
2016-07-17 22:43:46 +0800

    The 6 code files are created to build the Prog2

---
+++ How2Make/sort.h
@@ -0,0 +1,15 @@
+/*******************************************************************************
+ * File name    : sort.h
+ * Description  : 
+ * Author       : Frederick Hsu
+ * Creation date: Sun.  17 JULY, 2016
+ * Copyright(C) 2016    All rights reserved.
+ * 
+ *******************************************************************************/
+
+
+#ifndef SORT_H
+#define SORT_H
+
+
+#endif  /* SORT_H */
