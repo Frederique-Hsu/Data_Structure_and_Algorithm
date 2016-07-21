6f26d822257137e936f0155e19d3072bc43c9273
Frederick Hsu (徐赞) <frederick.hsu@icloud.com>
2016-07-21 14:56:06 +0800

    对双向链表 DoublyLinkedList的数据结构及其对链表的部分操作（方法函数）进行测试验证。 测试通过！

--- CPP_Data_Structures_and_Algorithms/Linked_Lists/Linked_Lists/Doubly_Linked_List/Test_DoublyLinkedList.cpp
+++ CPP_Data_Structures_and_Algorithms/Linked_Lists/Linked_Lists/Doubly_Linked_List/Test_DoublyLinkedList.cpp
@@ -10,10 +10,57 @@
 
 #include "Test_DoublyLinkedList.h"
 #include "DoublyLinkedList.h"
+#include <iostream>
 
 void testCase4_VerifyDoublyLinkedList(void)
 {
-    DoublyLinkedList<int> *pDLList = new DoublyLinkedList<int>();
-    pDLList->addToDoublyLinkedListHead();
+	int num = 0;
+
+	DoublyLinkedList<int> *pDLList = new DoublyLinkedList<int>(num);
+    num = 8;
+	pDLList->addToDoublyLinkedListHead(num);
+	pDLList->printDoublyLinkedList();
+
+	num = 1;
+	pDLList->addToDoublyLinkedListTail(num);
+	pDLList->printDoublyLinkedList();
+    
+    num = 10;
+    pDLList->addToDoublyLinkedListHead(num);
     pDLList->printDoublyLinkedList();
+    std::cout<<"Now this Doubly-Linked-List has "<<pDLList->numberOfNodes()<<" nodes."<<std::endl;
+
+    num = -9;
+    pDLList->addToDoublyLinkedListTail(num);
+    num = 95;
+    pDLList->addToDoublyLinkedListHead(num);
+    pDLList->printDoublyLinkedList();
+
+    num = 25;
+    pDLList->insertNewNodeAfterPositionN(num, 2);
+    pDLList->printDoublyLinkedList();
+    num = -30;
+    pDLList->insertNewNodeAfterPositionN(num, 5);
+    pDLList->insertNewNodeAfterPositionN(num, 1);
+    pDLList->printDoublyLinkedList();
+    num = 200;
+    pDLList->insertNewNodeAfterPositionN(num, pDLList->numberOfNodes());
+    pDLList->printDoublyLinkedList();
+
+    num = 16;
+    pDLList->insertNewNodeBeforePositionN(num, 4);
+    pDLList->printDoublyLinkedList();
+    num = -56;
+    pDLList->insertNewNodeBeforePositionN(num, pDLList->numberOfNodes());
+    pDLList->insertNewNodeBeforePositionN(num, 7);
+    pDLList->printDoublyLinkedList();
+    num = 100;
+    pDLList->insertNewNodeBeforePositionN(num, 1);
+    pDLList->printDoublyLinkedList();
+
+    DoublyLinkedList<int> *pMyList = new DoublyLinkedList<int>(*pDLList);
+    pMyList->printDoublyLinkedList();
+    
+    delete pDLList;
+    delete pMyList;
 }
