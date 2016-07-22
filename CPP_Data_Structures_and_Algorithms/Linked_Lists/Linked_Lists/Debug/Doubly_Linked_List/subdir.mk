7b4a6b1a6c0f659c930b874dd255e31e4cee41dd
Frederick Hsu (徐赞) <frederick.hsu@icloud.com>
2016-07-22 20:04:14 +0800

    Update the makefile and Xcode project setting files.

---
+++ CPP_Data_Structures_and_Algorithms/Linked_Lists/Linked_Lists/Debug/Doubly_Linked_List/subdir.mk
@@ -0,0 +1,24 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+CPP_SRCS += \
+../Doubly_Linked_List/Test_DoublyLinkedList.cpp 
+
+OBJS += \
+./Doubly_Linked_List/Test_DoublyLinkedList.o 
+
+CPP_DEPS += \
+./Doubly_Linked_List/Test_DoublyLinkedList.d 
+
+
+# Each subdirectory must supply rules for building sources it contributes
+Doubly_Linked_List/%.o: ../Doubly_Linked_List/%.cpp
+	@echo 'Building file: $<'
+	@echo 'Invoking: GCC C++ Compiler'
+	g++ -O0 -g3 -Wall -c -fmessage-length=0 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@)" -o "$@" "$<"
+	@echo 'Finished building: $<'
+	@echo ' '
+
+
