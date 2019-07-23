2bde1117437f488d7e6cafe0b6c4091670078c0f
Frederique Hsu <frederique.hsu@outlook.com>
2019-07-23 22:43:44 +0800

    Create a new Visual Studio C++ console project : CaseStudy_Library

---
+++ LinkedList/CaseStudy_Library/Main.cpp
@@ -0,0 +1,18 @@
+/**************************************************************************************************
+ * Project name	: LinkedList
+ * Description	: Study the data structure of Linked-List in C++.
+ * ================================================================================================
+ * File name	: Main.cpp
+ * Description	: This file implements the main() function for current project.
+ * Creator		: Frederick Hsu
+ * Creation date: Tue.	23 July 2019
+ * Copyright(C)	2019	All rights reserved.
+ *
+ **************************************************************************************************/
+
+#include <iostream>
+
+int main(int argc, char* argv[])
+{
+	return 0;
+}
