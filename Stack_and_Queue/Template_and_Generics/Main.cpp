3d728d068418dff2c86d494335f9bbae643b6fe2
Frederique Hsu <frederique.hsu@outlook.com>
2019-08-06 01:29:22 +0800

    Create the Visual Studio C++ console application project : Template_and_Generics.vcxprj

---
+++ Stack_and_Queue/Template_and_Generics/Main.cpp
@@ -0,0 +1,18 @@
+/**************************************************************************************************
+ * Project name	: Template_and_Generics
+ * Description	: Investigation on the template and generic programming in C++.
+ * ================================================================================================
+ * File name	: Main.cpp
+ * Description	: This file implements the main() entry function for this project.
+ * Creator		: Frederick Hsu
+ * Creation date: Tue.	06 Aug. 2019
+ * Copyright(C)	2019	All rights reserved.
+ *
+ **************************************************************************************************/
+
+#include <iostream>
+
+int main(int argc, char* argv[])
+{
+	return 0;
+}
