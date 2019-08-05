724f63dbef18e328665ff1a6ea4fa2f0e2991029
Frederique Hsu <frederique.hsu@outlook.com>
2019-08-06 01:27:40 +0800

    Create the Visual Studio C++ console application project : Stacks.vcxprj under the solution of Stack_and_Queue.sln

---
+++ Stack_and_Queue/Stacks/Main.cpp
@@ -0,0 +1,20 @@
+/**************************************************************************************************
+ * Project name	: Stacks
+ * Description	: This project will delve the data structure of stack, and its operations.
+ * ================================================================================================
+ * File name	: Main.cpp
+ * Description	: This file implements the main() entry function for current project.
+ * Creator		: Frederick Hsu
+ * Creation date: Sun.	05 Aug. 2019
+ * Copyright(C)	2019	All rights reserved.
+ *
+ **************************************************************************************************/
+
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char* argv[])
+{
+	return 0;
+}
