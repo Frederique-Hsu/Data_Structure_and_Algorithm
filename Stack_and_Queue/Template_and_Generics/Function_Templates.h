9781be14c7bb270a203fb6d67a85e3faf0ee558b
Frederique Hsu <frederique.hsu@outlook.com>
2019-08-09 20:42:47 +0800

    Declare the function pointers of function template

--- Stack_and_Queue/Template_and_Generics/Function_Templates.h
+++ Stack_and_Queue/Template_and_Generics/Function_Templates.h
@@ -2,6 +2,8 @@
 #define FUNCTIONS_TEMPLATES_H
 
 	#include <ctype.h>
+	#include <string>
+	using namespace std;
 
 	template<typename Type>
 	int compare(const Type& v1, const Type& v2)
@@ -55,6 +57,15 @@
 	Type sum(const Type& op1, int op2)
 	{
 		return op1 + op2;
+	}
+
+	void func(int(*)(const string&, const string&));
+	void func(int(*)(const int&, const int&));
+
+	template<class T1, class T2, class T3>
+	T3 alternative_sum(T2 m, T3 n)
+	{
+		return (m + n);
 	}
 
 #endif	/* FUNCTIONS_TEMPLATES_H */
