c96b1a2d08ccd0a6db050d4d8660814d04cf4d14
Frederique Hsu <frederique.hsu@outlook.com>
2019-08-15 18:15:09 +0800

    Create a new Visual Studio C++ console application project : Handle.vcxproj, to delve the topic of handle class and inheritance.

---
+++ Stack_and_Queue/Handle/Main.cpp
@@ -0,0 +1,16 @@
+/**************************************************************************************************
+ * Project name	: Handle
+ * Description	: Investigate the topic of handle class and inheritance.
+ * ================================================================================================
+ * File name	: Main.cpp
+ * Description	: This file implements the main() entry function for current project.
+ * Creator		: Frederique Hsu
+ * Creation date: Wed.	14 Aug. 2019
+ * Copyright(C)	2019	All rights reserved.
+ *
+ **************************************************************************************************/
+
+int main(int argc, char* argv[])
+{
+	return 0;
+}
