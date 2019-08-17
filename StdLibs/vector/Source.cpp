800b158dece9ab826d68db55a347644311b0969e
Frederique Hsu <frederique.hsu@outlook.com>
2019-08-18 02:56:01 +0800

    Start to study the standard vector type.

---
+++ StdLibs/vector/Source.cpp
@@ -0,0 +1,16 @@
+/**************************************************************************************************
+ * Project name : vector
+ * Description  : Study the standard vector type.
+ * ================================================================================================
+ * File name    : Source.cpp
+ * Description  : This file implements the main() entry function for current project.
+ * Creator      : Frederick Hsu
+ * Creation date: Sun.  18 Aug. 2019
+ * Copyright(C) 2019    All rights reserved.
+ *
+ **************************************************************************************************/
+
+int main(int argc, char* argv[])
+{
+    return 0;
+}
