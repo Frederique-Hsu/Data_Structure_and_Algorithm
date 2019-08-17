2ba0a8653d3386b9ad40fcc26d9a883c39c2487b
Frederique Hsu <frederique.hsu@outlook.com>
2019-08-18 03:01:16 +0800

    Study how to initialize or instantiate the vector<typename> objects.

---
+++ StdLibs/vector/Vector.cpp
@@ -0,0 +1,14 @@
+#include "Vector.h"
+
+#include <vector>
+using std::vector;
+
+void execute_test_cases(void)
+{
+    instantiate_vector_objects();
+}
+
+void instantiate_vector_objects(void)
+{
+    vector<int> ivec;   /* ivec holds objects of type int */
+}
---
+++ StdLibs/vector/Vector.h
@@ -0,0 +1,8 @@
+#ifndef VECTOR_H
+#define VECTOR_H
+
+    void execute_test_cases(void);
+
+    void instantiate_vector_objects(void);
+
+#endif  /* VECTOR_H */
