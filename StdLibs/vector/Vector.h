cd298aa6c1fd4901449de789e5c65dd38298654d
Frederique Hsu <frederique.hsu@outlook.com>
2019-08-21 22:44:12 +0800

    Implement 2 more test case functions : add_element_into_vector(void);  access_elements_through_iterator(void);

--- StdLibs/vector/Vector.h
+++ StdLibs/vector/Vector.h
@@ -4,5 +4,7 @@
     void execute_test_cases(void);
 
     void instantiate_vector_objects(void);
+    void add_element_into_vector(void);
+    void access_elements_through_iterator(void);
 
 #endif  /* VECTOR_H */
