e53a6b4c76bf97b74ec67eaf4b397aacb9aa9543
Frederique Hsu <frederique.hsu@outlook.com>
2019-08-21 22:55:19 +0800

    Create a C++ console application project : bitset, in order to study the subject of standard bitset type.

---
+++ StdLibs/bitset/source.cpp
@@ -0,0 +1,17 @@
+/**************************************************************************************************
+ * Project name     : source.cpp
+ * Description      : Study the standard type of bitset
+ * ================================================================================================
+ * File name        : source.cpp
+ * Description      : This file implements the main() function for current project.
+ * Creator          : Frederick Hsu
+ * Creation date    : Wed.  21 Aug. 2019
+ * Copyright(C)     2019    All rights reserved.
+ *
+ **************************************************************************************************/
+
+
+int main(int argc, char* argv[])
+{
+    return 0;
+}
