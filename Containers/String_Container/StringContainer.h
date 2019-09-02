3b6e78477aec96c3c2c9a92861ada8e6b8082f70
Frederique Hsu <frederique.hsu@outlook.com>
2019-09-02 23:53:45 +0800

    1. Study the string constructor methods 2. How to access the string objects, by using these methods: 	string.erase(), 	string.substr(), 	string.assign(), 	string.insert(), 	string.append(), 	string.replace(), 	string.find(), 	string.rfind(), 	string.find_first_of(), 	string.find_last_of(), 	string.find_first_not_of(), 	string.find_last_not_of()

--- Containers/String_Container/StringContainer.h
+++ Containers/String_Container/StringContainer.h
@@ -4,5 +4,8 @@
     void execute_test_cases(void);
 
     void access_string_iterator(void);
+    void modify_string_object(void);
+    void operations_on_string(void);
+    void compare_string_objects(void);
 
 #endif  /* STRING_CONTAINER_H */
