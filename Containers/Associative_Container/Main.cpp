994c44bb1512fd9e6cc8731c60e64d4b090b1173
Frederique Hsu <frederique.hsu@outlook.com>
2019-09-03 03:12:42 +0800

    Create a new console application project : Associative_Container, we want to study the subject of associative container in-depth.
    Signed-off-by: Frederique Hsu <frederique.hsu@outlook.com>

---
+++ Containers/Associative_Container/Main.cpp
@@ -0,0 +1,18 @@
+/**************************************************************************************************
+ * Project      : Associative_Container
+ * Description  : Research the subject of associative container in-depth.
+ * ================================================================================================
+ * File name    : Main.cpp
+ * Description  : This file implements the main() entry function for current project.
+ * Creator      : Frederick Hsu
+ * Creation date: Tue.  03 Sep. 2019
+ * Copyright(C) 2019    All rights reserved.
+ *
+ **************************************************************************************************/
+
+#include <cstdio>
+
+int main(int argc, char* argv[])
+{
+    return std::getchar();
+}
