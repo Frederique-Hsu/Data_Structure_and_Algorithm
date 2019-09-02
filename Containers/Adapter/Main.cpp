cada34f0bf37565d9a839dfb5e4a8919dba4aefc
Frederique Hsu <frederique.hsu@outlook.com>
2019-09-03 00:02:59 +0800

    Create a new console project : Adapter.vcxproj to study the adapter subject of STL container.

---
+++ Containers/Adapter/Main.cpp
@@ -0,0 +1,20 @@
+/**************************************************************************************************
+ * Project      : Adpater
+ * Description  : Study the container's adapter concept
+ * ================================================================================================
+ * File name    : Main.cpp
+ * Description  : This file implements the main() entry function for current project.
+ * Creator      : Frederick Hsu
+ * Creation date: Mon.  02 Sep. 2019
+ * Copyright(C) 2019    All rights reserved.
+ *
+ **************************************************************************************************/
+
+#include <cstdio>
+#include "Adapter.h"
+
+int main(int argc, char* argv[])
+{
+    execute_test_cases();
+    return std::getchar();
+}
