32aedede7e4ce7881ea236b73b60d590951e3e4f
Frederique Hsu <frederique.hsu@outlook.com>
2019-09-06 02:38:28 +0800

    Create a solution Generic_Algorithms.sln and a new console application project Algorithm_Basics.vcxproj
    to study the subject of generic algorithms in the standard libraries.

---
+++ Generic_Algorithms/Algorithm_Basics/testing.cpp
@@ -0,0 +1,15 @@
+#include "testing.h"
+#include "Generic_Algorithm_Basics.h"
+#include "Sorting_Algorithms.h"
+
+#include "interview.h"
+
+void execute_test_cases(void)
+{
+    // readonly_algorithm();
+    // write_container_elements_algorithm();
+    // write_into_target_iterators();
+    // sort_elements_inside_container();
+    // verify();
+    verify_unique_sort_random_numbers();
+}
