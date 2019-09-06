730d7f3e32e09bdff53b1b51398559f734a7533f
Frederique Hsu <frederique.hsu@outlook.com>
2019-09-06 23:42:01 +0800

    Declare and implement a new function : void restricted_wc(std::ifstream& remove_file, std::map<std::string, int>& word_count);

--- Containers/Associative_Container/Set.cpp
+++ Containers/Associative_Container/Set.cpp
@@ -21,8 +21,35 @@ void utilize_set_container(void)
     cout << "The size of iset is: " << iset.size() << endl;
     set<int> intset;
     intset.insert(ivec.begin(), ivec.end());
+    set<int>::iterator it = intset.find(5);
+    set<int>::size_type count = intset.count(8);
 
     set<string> strset;
     strset.insert("the");
     strset.insert("and");
+    pair<set<string>::iterator, bool> setpair = strset.insert("the");
+    if (setpair.second == false)
+    {
+        cout << "The key \"" << *(setpair.first) << "\" was actually not inserted into set." << endl;
+    }
+}
+
+void restricted_wc(ifstream& remove_file, map<string, int>& word_count)
+{
+    set<string> excluded;   /* set to hold words we will ignore */
+    string remove_word;
+    while (remove_file >> remove_word)
+    {
+        excluded.insert(remove_word);
+    }
+    /* read input and keep a count for words that are not in the exclusion set */
+    string word;
+    while (cin >> word)
+    {
+        /* increment counter only if the word is not in excluded */
+        if (!excluded.count(word))
+        {
+            ++word_count[word];
+        }
+    }
 }
