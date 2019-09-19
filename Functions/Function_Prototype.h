73ad7d74899728843a15b91e89b10ded85a91b46
Frederique Hsu <frederique.hsu@outlook.com>
2019-09-19 22:23:06 +0800

    Implement more functions.

--- Functions/Function_Prototype.h
+++ Functions/Function_Prototype.h
@@ -2,9 +2,10 @@
 #define FUNCTION_PROTOTYPE_H
 
     #include <string>
+    #include <vector>
     using namespace std;
 
-    // #define REFERENCE_PARAMETERS
+    #define REFERENCE_PARAMETERS
 
     int gcd(int v1, int v2);
     bool is_present(int*, int);
@@ -25,5 +26,31 @@
 #else
     void swap(int& v1, int& v2);
 #endif
+    void ptrswap(int *&v1, int *&v2);
+
+    vector<int>::const_iterator find_val(vector<int>::const_iterator beg,
+                                         vector<int>::const_iterator end,
+                                         int value,
+                                         vector<int>::size_type& occurs);
+    bool isShorter(const string& s1, const string& s2);
+    int incr(int &val);
+    string::size_type find_char(const string& s, char c);
+    bool is_sentence(const string& s);
+    void print(vector<int>::const_iterator beg, vector<int>::const_iterator end);
+
+    /* array as the parameter to function
+     * three equivalent definitions of printValues
+     */
+    void printValues(int*);
+    void printValues(int[]);
+    void printValues(int[10]);
+    void printValues(const int iarr[], size_t size);
+    void printValues(int (&arr)[10]);
+    void printValues(int (*matrix)[10], int rowSize);   /* first parameter is an array, whose elements are arrays of 10 ints */
+    void printValues(int matrix[][10], int rowSize);
+
+    string make_plural(size_t ctr, const string& word, const string& ending);
+    const string& shorterString(const string& s1, const string& s2);
+    char& get_val(string& str, string::size_type pos);
 
 #endif  /* FUNCTION_PROTOTYPE_H */
