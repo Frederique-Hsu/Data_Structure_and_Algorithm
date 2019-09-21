5ea497232cf38c794e49717c302721d4cb3f2b86
Frederique Hsu <frederique.hsu@outlook.com>
2019-09-22 01:23:18 +0800

    Design test cases to verify the user-defined class "Sales_item".

--- Utility_Classes_Libraries/Unit_Testing/unit_testing.h
+++ Utility_Classes_Libraries/Unit_Testing/unit_testing.h
@@ -3,5 +3,8 @@
 
     void execute_test_cases(void);
     void instantiateObjects();
+    void const_overload();
+    void callConstructor();
+    void copyControl();
 
 #endif  /* UNIT_TESTING_H */
