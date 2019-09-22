73a0234d13adedbda3c66d6420d374f50eb64ac7
Frederique Hsu <frederique.hsu@outlook.com>
2019-09-22 15:50:00 +0800

    Define and implement the class "Message".

--- Utility_Classes_Libraries/Sales_Item/Message.cpp
+++ Utility_Classes_Libraries/Sales_Item/Message.cpp
@@ -1,1 +1,46 @@
 #include "Message.h"
+#include "Folder.h"
+
+Message::Message(const string& str) : contents(str)
+{
+}
+
+Message::Message(const Message& orig) : contents(orig.contents), folders(orig.folders)
+{
+    put_Msg_in_Folders(folders);
+}
+
+void Message::put_Msg_in_Folders(const set<Folder *> &ptrfolders)
+
+{
+    for (set<Folder*>::const_iterator it = ptrfolders.begin(); it != ptrfolders.end(); ++it)
+    {
+        (*it)->addMsg(this);
+    }
+}
+
+Message& Message::operator=(const Message &other)
+{
+    if (&other != this)
+    {
+        remove_Msg_from_Folders();
+        contents = other.contents;
+        folders = other.folders;
+        put_Msg_in_Folders(other.folders);
+    }
+    return *this;
+}
+
+void Message::remove_Msg_from_Folders()
+{
+    /* remove this message from corresponding folders */
+    for (set<Folder*>::const_iterator it = folders.begin(); it != folders.end(); ++it)
+    {
+        (*it)->remMsg(this);
+    }
+}
+
+Message::~Message()
+{
+    remove_Msg_from_Folders();
+}
