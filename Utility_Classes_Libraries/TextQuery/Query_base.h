c424a6e3137e266c0cb5086b3e964adfc0ddd6b5
Frederique Hsu <frederique.hsu@outlook.com>
2019-10-03 02:04:40 +0800

    Define the root abstract class "Query_base", it is an interface.

---
+++ Utility_Classes_Libraries/TextQuery/Query_base.h
@@ -0,0 +1,24 @@
+#ifndef QUERY_BASE_H
+#define QUERY_BASE_H
+
+    #include "TextQuery.h"
+
+    #include <iostream>
+    using namespace std;
+
+    class Query_base
+    {
+        friend class Query;
+    public:
+    protected:
+        typedef TextQuery::line_no line_no;
+        virtual ~Query_base();
+    private:
+        /* eval returns the |set| of lines that this Query matches */
+        virtual set<line_no> eval(const TextQuery&) const = 0;
+        /* display prints the query */
+        virtual ostream& display(ostream& = cout) const = 0;
+    };
+
+
+#endif  /* QUERY_BASE_H */
