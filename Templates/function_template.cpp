06b47554ea59fbd186989892d037cb5fc35eb383
Frederique Hsu <frederique.hsu@outlook.com>
2019-10-06 17:28:13 +0800

    Study the function templates.

--- Templates/function_template.cpp
+++ Templates/function_template.cpp
@@ -16,3 +16,36 @@ template<class T, size_t N> void array_init(T (&parm)[N])
         parm[index] = 0;
     }
 }
+
+template<class T1, class T2, class T3> T1 sum(T2 a, T3 b)
+{
+    return (a + b);
+}
+
+template<class T1, class T2, class T3> T3 alternative_sum(T2 a, T1 b)
+/* poor design: Users must explicitly specify all 3 template parameters */
+{
+    return (a + b);
+}
+
+template<size_t N> void generate_fibonacci(unsigned (&fib)[N])
+{
+    fib[0] = 1;
+    fib[1] = 1;
+
+    for (int index = 2; index < N; ++index)
+    {
+        fib[index] = fib[index-1] + fib[index-2];
+    }
+}
+
+template<size_t N> void generate_fibonacci(vector<size_t>& fib)
+{
+    fib.push_back(1);
+    fib.push_back(1);
+
+    for (int index = 2; index < N; ++index)
+    {
+        fib.push_back(fib[index-1] + fib[index-2]);
+    }
+}
--- Templates/function_template.h
+++ Templates/function_template.h
@@ -10,6 +10,10 @@
 #ifndef FUNCTION_TEMPLATE_H
 #define FUNCTION_TEMPLATE_H
 
+    #include <cstddef>
+    #include <vector>
+    using namespace std;
+
     template<typename T> int compare(const T& v1, const T& v2)
     {
         if (v1 < v2)
@@ -23,6 +27,10 @@
         return 0;
     }
 
+    int (*pcomp)(const int&, const int&) = compare;
+
+    void fnct(int(*) (const int&, const int&));
+
     template<typename T> inline T min(const T& v1, const T& v2)
     {
         if (v1 <= v2)
@@ -33,8 +41,13 @@
 
     template<class Type> Type calc(const Type& a, const Type& b);
     template<class T, size_t N> void array_init(T (&parm)[N]);
+    template<class T1, class T2, class T3> T1 sum(T2 a, T3 b);
+    template<class T1, class T2, class T3> T3 alternative_sum(T2 a, T1 b);
+
+    template<size_t N> void generate_fibonacci(unsigned (&fib)[N]);
+    template<size_t N> void generate_fibonacci(vector<size_t>& fib);
 
 
-    #include "function_template.cpp"
+    #include "function_template.cpp"    /* inclusion compilation model */
 
 #endif  /* FUNCTION_TEMPLATE_H */
