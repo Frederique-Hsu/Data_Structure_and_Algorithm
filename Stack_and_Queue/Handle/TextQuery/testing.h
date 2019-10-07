926a538ec82f34dc1eba48076e0784d5ccbc8633
Frederique Hsu <frederique.hsu@outlook.com>
2019-10-07 16:28:41 +0800

    Remove the code files under the directories: Stack_and_Queue/Handle, /Template_and_Generics

--- Stack_and_Queue/Handle/TextQuery/testing.h
+++
@@ -1,6 +0,0 @@
-#ifndef TESTING_H
-#define TESTING_H
-
-	int executeTextQuery(int argc, char* argv[]);
-
-#endif	/* TESTING_H */
--- Stack_and_Queue/Template_and_Generics/Screen.cpp
+++
@@ -1,3 +0,0 @@
-#include "Screen.h"
-
-Screen<24, 80> hp2621;		/* screen 24 lines by 80 characters*/
