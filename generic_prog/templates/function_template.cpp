24d93b44dbebe0db6b4cab824d8405fece2385be
Frederique Hsu <frederique.hsu@outlook.com>
2020-03-29 18:37:45 +0800

    Create a new project "templates" and its Makefile script, to study the generic programming with templates.

---
+++ generic_prog/templates/function_template.cpp
@@ -0,0 +1,45 @@
+// #include "function_template.h"
+
+#include <cstddef>
+#include <vector>
+
+using namespace std;
+
+template<typename Type> int compare(const Type& v1, const Type& v2)
+{
+    if (v1 < v2)
+        return -1;
+    if (v2 < v1)
+        return 1;
+    return 0;
+}
+
+template<typename T, size_t N> void array_init(T (&parm)[N])
+{
+    for (size_t index = 0; index != N; ++index)
+    {
+        parm[index] = 0;
+    }
+}
+
+template<size_t N> void fibonacci_series(unsigned (&fib)[N])
+{
+    fib[0] = 1;
+    fib[1] = 1;
+
+    for (size_t index = 2; index < N; ++index)
+    {
+        fib[index] = fib[index-1] + fib[index-2];
+    }
+}
+
+template<size_t N> void fibonacci_series(vector<size_t> &fib)
+{
+    fib.push_back(1);
+    fib.push_back(1);
+
+    for (size_t index = 2; index < N; ++index)
+    {
+        fib.push_back(fib[index-1] + fib[index-2]);
+    }
+}
---
+++ generic_prog/templates/main.cpp
@@ -0,0 +1,18 @@
+/**************************************************************************************************
+ * Project name : templates
+ * Description  : Study the generic programming techniques, how to utilize the templates in C++.
+ * ================================================================================================
+ * File name    : main.cpp
+ * Description  : This file will implements the main() entry function for current project.
+ * Creator      : Frederique Hsu
+ * Creation date: Sun.  29 March 2020
+ * Copyright(C) 2020    All rights reserved.
+ *
+ **************************************************************************************************/
+
+#include <iostream>
+
+int main(int argc, char* argv[])
+{
+    return 0;
+}
