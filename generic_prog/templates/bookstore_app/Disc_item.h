5b11e47debf99120a2003be9d7ab5575a84e366d
Frederique Hsu <frederique.hsu@outlook.com>
2020-03-31 19:18:26 +0800

    Define and implement the abstract class "Disc_item".

---
+++ generic_prog/templates/bookstore_app/Disc_item.h
@@ -0,0 +1,30 @@
+#ifndef DISC_ITEM_H
+#define DISC_ITEM_H
+
+    #include "Item_base.h"
+    #include <utility>
+
+    namespace BookStore_App
+    {
+        class Disc_item;
+    }
+
+    class Disc_item : public Item_base
+    {
+    public:
+        Disc_item(const string& book = "",
+                  double sales_price = 0.00,
+                  size_t qty = 0,
+                  double disc_rate = 0.00);
+        Disc_item(const Disc_item& orig);
+        Disc_item& operator=(const Disc_item& orig);
+        ~Disc_item();
+    public:
+        pair<size_t, double> discount_policy() const;
+        double net_price(size_t) const = 0;
+    protected:
+        size_t quantity;
+        double discount;
+    };
+
+#endif  /* DISC_ITEM_H */
