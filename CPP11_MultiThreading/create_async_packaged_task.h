49d77f2a55dafce6347b13034552e22f4e430e59
Frederique Hsu <frederique.hsu@outlook.com>
2020-04-23 02:59:54 +0800

    Part 10: packaged_task<> Example and tutorial.

---
+++ CPP11_MultiThreading/create_async_packaged_task.h
@@ -0,0 +1,9 @@
+#ifndef CREATE_ASYNC_PACKAGED_TASK_H
+#define CREATE_ASYNC_PACKAGED_TASK_H
+
+    #include <string>
+    using namespace std;
+
+    string getDataFromDB(string token);
+    void execute_async_packaged_task(void);
+#endif  /* CREATE_ASYNC_PACKAGED_TASK_H */
