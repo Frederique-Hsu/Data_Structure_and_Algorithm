e9df71e9b9427329c8d2761150fb2b8c4fc54424
Frederique Hsu <frederique.hsu@outlook.com>
2020-04-23 00:56:18 +0800

    Part 2: Joining and detaching threads.

---
+++ CPP11_MultiThreading/program.cpp
@@ -0,0 +1,29 @@
+/* Fiel name    : program.cpp
+ * Description  : This file implement the main() entry function for current project.
+ *
+ */
+
+#include <iostream>
+
+#include "join_detach_thread.h"
+#include "ThreadRAII.h"
+#include "pass_args_to_threads.h"
+#include "race_conditions.h"
+#include "event_handling.h"
+
+int main(int argc, char* argv[])
+{
+    std::cout << "C++ standard no.: " << __cplusplus << std::endl;
+
+    // join_threads();
+    // detach_threads();
+    // create_safe_thread();
+    // pass_simple_arguments_to_threads();
+    // not_pass_pointer_argument_to_threads();
+    // pass_reference_arguments_to_threads();
+    // pass_memberFunction_arguments_to_threads();
+    // verifyMultiThreadConflict();
+    handle_event();
+
+    return 0;
+}
