764445256c8aba7d5939f013dadbfc875004923a
Frederique Hsu <frederique.hsu@outlook.com>
2024-09-03 00:24:44 +0800

    Add the Stack project.

--- Stack/main.c
+++ Stack/main.c
@@ -1,17 +1,22 @@
 /*!
  *  \file       main.c
- *  \brief      
- *  
+ *  \brief
+ *
  */
 
 
 #include <stdio.h>
-#include <stdlib.h>
+
+#include <unity/unity_fixture.h>
+
+static void RunAllTests(void)
+{
+}
 
 int main(int argc, char* argv[])
 {
     printf("Current C standard no. is %ld\n", __STDC_VERSION__);
     printf("Current C program is %ld-bits.\n", sizeof(void*) * 8);
-    
-    return EXIT_SUCCESS;
+
+    return UnityMain(argc, (const char**)argv, RunAllTests);
 }
